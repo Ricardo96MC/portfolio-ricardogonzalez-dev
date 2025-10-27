4f9a5e7faaa8c6fc0bb49336dbbbb55161acd5a6
Ricardo Gonzalez <ismaelcod4@gmail.com>
2025-10-26 19:15:12 -0500

    Updated project section to pull from DB!

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,28 +1,50 @@
 import React from 'react'
 import ProjectCard from './ProjectCard'
+import { supabaseServer } from '@/lib/supabaseServerClient'
 
-const tempTechn = ["Java", "Spring", 'PostgreSQL']
+export default async function Project() {
+    /**
+     * We have stored our Project information in Supabase,
+     * so lets fetch them! Also we have images in a bucket
+     * so we will access them via a signed url
+     */
+    const { data: projects, error } = await supabaseServer
+        .from('projects')
+        .select('*');
 
-const tempObect = {
-    projectName: "EdgePingService",
-    projectDetails: "Lorem Ipsum \n\n\n\n LoremIpsum",
-    technology: tempTechn,
-    thumbnail: "/EdgePingService.png",
-}
+    if (error) {
+        console.log("Error fetching projects from DB: ", error);
+        return (
+            <div>Error loading projects.</div>
+        )
+    }
+    const projectsWithImages = await Promise.all(
+        projects.map(async (proj) => {
+            const { data: signed } = await supabaseServer
+                .storage
+                .from('portfolio-images')
+                .createSignedUrl(proj.thumbnail_url, 60 * 60); //Url is valid for 1 hr
+            return {
+                ...proj,
+                signed_thumbnail: signed?.signedUrl ?? null,
+            }
+        })
+    );
 
-const Project = () => {
+
     return (
         <section className="flex flex-col items-center justify-center text-left text-[#EAEAEA]">
             <div className="max-w-3xl w-[90%] md:w-[600px] lg:w-1/2 bg-[#1A1A1A] rounded-2xl shadow-md p-8 border border-[#2A2A2A]">
                 <h1 className="text-5xl mb-12 text-center">
-                    <a href="/experience" className="hover:text-[#B22222]">
+                    <a href="/projects" className="hover:text-[#B22222]">
                         <span>Projects</span>
                     </a>
                 </h1>
-                <ProjectCard {...tempObect} />
+                {projectsWithImages.map((proj) => (
+                    <ProjectCard key={proj.id} project={proj} />
+                ))}
             </div>
         </section>
     )
 }
 
-export default Project
